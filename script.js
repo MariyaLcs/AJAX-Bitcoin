3a1ae70d1d2ab9ff072189c12f340ac206924af0
Mariya <mariya.podosinova@gmail.com>
2020-04-29 13:11:25 +0100

    added currency

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 let btn = document.querySelector("button"),
-  priceDisp = document.querySelector("#price");
+  priceDisp = document.querySelector("#price"),
+  radios = document.getElementsByName("currency"),
+  hide = document.getElementsByClassName("hide");
 
 btn.addEventListener("click", function () {
   let XHR = new XMLHttpRequest();
 
   XHR.onreadystatechange = function () {
-    if (XHR.readyState == 4 && XHR.status == 200) {
-      let data = JSON.parse(XHR.responseText);
-      let price = data.bpi.GBP.rate;
-      priceDisp.innerText = price + "GBP";
+    for (var i = 0, length = radios.length; i < length; i++) {
+      if (radios[i].checked) {
+        if (radios[i].value === "USD") {
+          url = JSON.parse(XHR.responseText).bpi.USD.rate;
+          price.innerText = "$" + url + " USD";
+        }
+        if (radios[i].value === "GBP") {
+          url = JSON.parse(XHR.responseText).bpi.GBP.rate;
+          price.innerText = "£" + url + " GBP";
+        }
+        if (radios[i].value === "EUR") {
+          url = JSON.parse(XHR.responseText).bpi.EUR.rate;
+          price.innerText = "€" + url + " EUR";
+        }
+        break;
+      }
     }
   };
 
